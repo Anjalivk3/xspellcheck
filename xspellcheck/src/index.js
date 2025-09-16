cffc177d3706f90b42df19c82c9c7e5fb9ae3c7f
Anjali jain <rohini1jb@gmail.com>
2025-09-16 22:46:22 +0530

    updated

--- xspellcheck/src/index.js
+++ xspellcheck/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import SpellCheckApp from './SpellCheckApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SpellCheckApp />
   </React.StrictMode>
 );
 
